7c5eff2f734c5f6256baf0ea86e3927f050ac18d
PeeHaa <info@pieterhordijk.com>
2012-09-25 20:52:50 +0200

    Moved shuffle to separate file

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,3 @@
-// Shuffle characters in a string
-String.prototype.shuffle = function ()
-{
-    var characters = this.split(''),
-        iterations = (characters.length-1),
-        i, j, tmp;
-
-    for (i = iterations; i > 0; i--) {
-        j = Math.floor(Math.random() * (i + 1));
-        tmp = characters[i];
-        characters[i] = characters[j];
-        characters[j] = tmp;
-    }
-
-    return characters.join('');
-}
-
-
 // Set global variables
 function setupVisited() {
   var arr = [];
