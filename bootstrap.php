129027f8a39a6a93b2a9e6563bdc727081d2b19a
PeeHaa <info@pieterhordijk.com>
2012-09-25 20:48:45 +0200

    Implemented debug and production mode

--- bootstrap.php
+++ bootstrap.php
@@ -7,6 +7,17 @@ error_reporting(-1);
 ini_set('display_errors', 1);
 
 /**
+ * Setup environment constants
+ */
+define('RPG_ENV_DEBUG', 1);
+define('RPG_ENV_PRODUCTION', 2);
+
+/**
+ * Setup environment
+ */
+define('RPG_ENV', RPG_ENV_DEBUG);
+
+/**
  * Display HTML
  */
 require '/views/index.phtml';
