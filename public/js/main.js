420786deb83b4e73ccef2567cd0765f700b1e851
PeeHaa <info@pieterhordijk.com>
2012-09-25 19:53:49 +0200

    Better naming for shuffle function

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,18 @@
 // Shuffle characters in a string
 String.prototype.shuffle = function ()
 {
-    var a = this.split(''), n = a.length;
-    for(var i = n - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var tmp = a[i];
-        a[i] = a[j];
-        a[j] = tmp;
+    var characters = this.split(''),
+        iterations = (characters.length-1),
+        i, j, tmp;
+
+    for (i = iterations; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        tmp = characters[i];
+        characters[i] = characters[j];
+        characters[j] = tmp;
     }
-    return a.join('');
+
+    return characters.join('');
 }
 
 
