0300f66b58811ff47036b3abf73f2ef7f61ebdbd
PeeHaa <info@pieterhordijk.com>
2012-09-28 02:03:33 +0200

    Implement reset and update

--- public/js/Debugger.js
+++ public/js/Debugger.js
@@ -1,7 +1,29 @@
 function Debugger() {
+  this.resetAllTiles = function($playfield) {
+    $('img.tile', $playfield).css({
+      opacity: 1
+    });
+  };
+
   this.resetTile = function($tile) {
     $tile.css({
       opacity: 1
     });
-  }
+  };
+
+  this.updateTile = function($tile) {
+    var currentOpacity = $tile.css('opacity');
+
+    if (currentOpacity > 0.14) {
+      var newOpacity = Math.round((currentOpacity - 0.05) * 100) / 100;
+
+      if (currentOpacity == 1) {
+        newOpacity = 0.5;
+      }
+
+      $tile.css({
+        opacity: newOpacity
+      });
+    }
+  };
 }
